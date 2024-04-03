790f1faa5ee6a65f95a59d8da2557f4a5ccc56ab
resme <resmerdk@gmail.com>
2024-04-03 14:01:23 +0300

    start lab

---
+++ src/main/java/uni/crossplatformlab3/CrossPlatformLab3Application.java
@@ -0,0 +1,13 @@
+package uni.crossplatformlab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformLab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossPlatformLab3Application.class, args);
+    }
+
+}
